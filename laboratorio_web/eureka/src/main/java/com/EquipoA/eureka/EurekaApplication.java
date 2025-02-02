6a05a29f62a56128de0b6b627fc54f8cc32352dd
hectorsp10 <sanchezpendas2000@gmail.com>
2025-02-02 15:22:52 +0100

    Recreated Eureka project with correct configuration

--- laboratorio_web/eureka/src/main/java/com/EquipoA/eureka/EurekaApplication.java
+++ laboratorio_web/eureka/src/main/java/com/EquipoA/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.equipo_a.eureka;
+package com.EquipoA.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
-
 }
--- laboratorio_web/eureka/src/test/java/com/EquipoA/eureka/EurekaApplicationTests.java
+++ laboratorio_web/eureka/src/test/java/com/EquipoA/eureka/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.equipo_a.eureka;
+package com.EquipoA.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
